3b0a3f3d373f3d1009f449d0ba6f68c8e7c8453f
Akhmedov-Asad <akhmedovasadbek76@gmail.com>
2023-02-04 20:15:31 +0500

    figures

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// let switchCase = + prompt("son kiriting");
+// switch (switchCase) {
+//     case 1: console.log("Dushanbi");
+//         break;
+//     case 2: console.log("Seshanbi");
+//         break;
+//     case 3: console.log("Chorshanbi");
+//         break;
+//     case 4: console.log("Payshanba");
+//         break;
+//     case 5: console.log("Juma");
+//         break;
+//     case 6: console.log("Shanbi");
+//         break;
+//     case 7: console.log("Yakshanbi");
+//         break;
+//     default: console.log("Boshqa son");
+//         break;
+// }  
+
+// var btnContainer = document.getElementById("myDIV");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
+
+
+// let arr = ['apple', 'banana', 'mango'];
+// arr.push('cherry');
+// console.log(arr[0]);
